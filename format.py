aca72cbf7e740db7ce627b1d39a2ab916d59765f
Technisha <proxneon960@gmail.com>
2020-11-28 13:50:50 +0000

    autocommit

---
+++ format.py
@@ -0,0 +1,8 @@
+from os import system, path, walk
+
+for base, _, files in walk("."):
+    if ".git" not in base:
+        for file in files:
+            system(f"git add {path.join(base, file)}")
+
+system("git commit -am 'autocommit';git push")
